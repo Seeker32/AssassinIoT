cf8e65cadf4ee5369a79991339e233a3970b2603
Wenkang Zhu <zhuwenkang@shinetechsoftware.com>
2026-06-01 14:58:39 +0800

    feat: enhance server shutdown handling and dependency closure

--- backend/internal/application/application.go
+++ backend/internal/application/application.go
@@ -2,6 +2,7 @@ package application
 
 import (
 	"context"
+	"errors"
 	"log/slog"
 	"net/http"
 	"os"
@@ -21,6 +22,8 @@ type server struct {
 	dep            dependence.Dep
 	logger         *slog.Logger
 	listenAndServe func(*http.Server) error
+	shutdown       func(*http.Server) error
+	signals        func() (<-chan os.Signal, func())
 }
 
 func NewServer(dep dependence.Dep) *server {
@@ -43,27 +46,23 @@ func (s *server) Start() error {
 		errCh <- s.runHTTPServer(httpServer)
 	}()
 
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-	defer signal.Stop(sigCh)
+	sigCh, stopSignals := s.signalSource()
+	defer stopSignals()
 
 	select {
 	case err := <-errCh:
-		if err == nil || err == http.ErrServerClosed {
-			return nil
-		}
-		return err
+		return s.closeDependencies(normalizeServerError(err))
 	case sig := <-sigCh:
 		s.logger.Info("shutting down server", "signal", sig.String())
 		ctx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
 		defer cancel()
-		if err := httpServer.Shutdown(ctx); err != nil {
-			return err
+		err := s.shutdownHTTPServer(ctx, httpServer)
+		if err == nil {
+			if serverErr := <-errCh; serverErr != nil && serverErr != http.ErrServerClosed {
+				err = serverErr
+			}
 		}
-		if err := <-errCh; err != nil && err != http.ErrServerClosed {
-			return err
-		}
-		return nil
+		return s.closeDependencies(err)
 	}
 }
 
@@ -80,4 +79,37 @@ func (s *server) runHTTPServer(httpServer *http.Server) error {
 		return s.listenAndServe(httpServer)
 	}
 	return httpServer.ListenAndServe()
+}
+
+func (s *server) signalSource() (<-chan os.Signal, func()) {
+	if s.signals != nil {
+		return s.signals()
+	}
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+
+	return sigCh, func() {
+		signal.Stop(sigCh)
+	}
+}
+
+func (s *server) shutdownHTTPServer(ctx context.Context, httpServer *http.Server) error {
+	if s.shutdown != nil {
+		return s.shutdown(httpServer)
+	}
+
+	return httpServer.Shutdown(ctx)
+}
+
+func (s *server) closeDependencies(err error) error {
+	return errors.Join(err, s.dep.Close())
+}
+
+func normalizeServerError(err error) error {
+	if err == nil || err == http.ErrServerClosed {
+		return nil
+	}
+
+	return err
 }
--- backend/internal/application/application_test.go
+++ backend/internal/application/application_test.go
@@ -5,13 +5,19 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
+	"os"
+	"reflect"
+	"syscall"
 	"testing"
 
+	"github.com/Seeker32/AssassinIoT/backend/ent"
 	"github.com/Seeker32/AssassinIoT/backend/internal/conf"
 	"github.com/Seeker32/AssassinIoT/backend/internal/dependence"
 )
 
 var errListenStub = errors.New("listen stub")
+var errCloseStub = errors.New("close stub")
+var errShutdownStub = errors.New("shutdown stub")
 
 type testConfigProvider struct {
 	serverConfig conf.ServerConfig
@@ -27,6 +33,32 @@ func (t testConfigProvider) ServerConfig() conf.ServerConfig {
 
 func testLogger() *slog.Logger {
 	return slog.New(slog.NewTextHandler(io.Discard, nil))
+}
+
+type testDep struct {
+	configProvider conf.ConfigProvider
+	logger         *slog.Logger
+	closeErr       error
+	events         *[]string
+}
+
+func (d *testDep) ConfigProvider() conf.ConfigProvider {
+	return d.configProvider
+}
+
+func (d *testDep) DBClient() *ent.Client {
+	return nil
+}
+
+func (d *testDep) Logger() *slog.Logger {
+	return d.logger
+}
+
+func (d *testDep) Close() error {
+	if d.events != nil {
+		*d.events = append(*d.events, "close")
+	}
+	return d.closeErr
 }
 
 func TestServerStartUsesConfiguredAddr(t *testing.T) {
@@ -78,5 +110,116 @@ func TestServerStartFallsBackToDefaultAddr(t *testing.T) {
 	}
 	if gotAddr != defaultServerAddr {
 		t.Fatalf("Start() addr = %q, want %q", gotAddr, defaultServerAddr)
+	}
+}
+
+func TestServerStartClosesDependenciesWhenListenReturns(t *testing.T) {
+	t.Helper()
+
+	events := []string{}
+	srv := NewServer(&testDep{
+		configProvider: testConfigProvider{},
+		logger:         testLogger(),
+		events:         &events,
+	})
+
+	srv.listenAndServe = func(httpServer *http.Server) error {
+		return errListenStub
+	}
+
+	err := srv.Start()
+	if !errors.Is(err, errListenStub) {
+		t.Fatalf("Start() error = %v, want error containing %v", err, errListenStub)
+	}
+	if !reflect.DeepEqual(events, []string{"close"}) {
+		t.Fatalf("events = %v, want [close]", events)
+	}
+}
+
+func TestServerStartReturnsJoinedListenAndCloseErrors(t *testing.T) {
+	t.Helper()
+
+	srv := NewServer(&testDep{
+		configProvider: testConfigProvider{},
+		logger:         testLogger(),
+		closeErr:       errCloseStub,
+	})
+
+	srv.listenAndServe = func(httpServer *http.Server) error {
+		return errListenStub
+	}
+
+	err := srv.Start()
+	if !errors.Is(err, errListenStub) {
+		t.Fatalf("Start() error = %v, want error containing %v", err, errListenStub)
+	}
+	if !errors.Is(err, errCloseStub) {
+		t.Fatalf("Start() error = %v, want error containing %v", err, errCloseStub)
+	}
+}
+
+func TestServerStartClosesDependenciesAfterSignalShutdown(t *testing.T) {
+	t.Helper()
+
+	events := []string{}
+	sigCh := make(chan os.Signal, 1)
+	shutdownDone := make(chan struct{})
+	sigCh <- syscall.SIGTERM
+
+	srv := NewServer(&testDep{
+		configProvider: testConfigProvider{},
+		logger:         testLogger(),
+		events:         &events,
+	})
+
+	srv.listenAndServe = func(httpServer *http.Server) error {
+		<-shutdownDone
+		return http.ErrServerClosed
+	}
+	srv.shutdown = func(httpServer *http.Server) error {
+		events = append(events, "shutdown")
+		close(shutdownDone)
+		return nil
+	}
+	srv.signals = func() (<-chan os.Signal, func()) {
+		return sigCh, func() {}
+	}
+
+	if err := srv.Start(); err != nil {
+		t.Fatalf("Start() error = %v, want nil", err)
+	}
+	if !reflect.DeepEqual(events, []string{"shutdown", "close"}) {
+		t.Fatalf("events = %v, want [shutdown close]", events)
+	}
+}
+
+func TestServerStartReturnsJoinedShutdownAndCloseErrors(t *testing.T) {
+	t.Helper()
+
+	sigCh := make(chan os.Signal, 1)
+	sigCh <- syscall.SIGTERM
+
+	srv := NewServer(&testDep{
+		configProvider: testConfigProvider{},
+		logger:         testLogger(),
+		closeErr:       errCloseStub,
+	})
+
+	srv.listenAndServe = func(httpServer *http.Server) error {
+		return http.ErrServerClosed
+	}
+	srv.shutdown = func(httpServer *http.Server) error {
+		return errShutdownStub
+	}
+	srv.signals = func() (<-chan os.Signal, func()) {
+		return sigCh, func() {}
+	}
+
+	err := srv.Start()
+	if !errors.Is(err, errShutdownStub) {
+		t.Fatalf("Start() error = %v, want error containing %v", err, errShutdownStub)
+	}
+	if !errors.Is(err, errCloseStub) {
+		t.Fatalf("Start() error = %v, want error containing %v", err, errCloseStub)
 	}
 }
